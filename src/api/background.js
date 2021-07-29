002279a942625f4d75b24da0a144197e80a9ade8
Josh Gerontis <gerontisj@yahoo.com>
2021-07-29 10:52:50 -0600

    small fixes in background.js

--- src/api/background.js
+++ src/api/background.js
@@ -3,7 +3,31 @@ var nodemailer = require("nodemailer");
 // var theSender = require("../../theSenderInfo");
 var ignore = require("../../ignoreMe.json");
 // const { google } = require("googleapis");
-var providerList = [ "sms.alltelwireless.com", "mms.alltelwireless.com", "txt.att.net", "mms.att.net", "sms.myboostmobile.com", "myboostmobile.com", "txt.att.net", "mms.att.net", "mymetropcs.com", "mymetropcs.com", "msg.fi.google.com", "text.republicwireless.com", "messaging.sprintpcs.com", "pm.sprint.com", "message.ting.com", "tmomail.net", "email.uscc.net", "mms.uscc.net", "vtext.com", "vzwpix.com", "mypixmessages.com", "vmobl.com", "vmpix.com"]
+var providerList = [
+  "sms.alltelwireless.com",
+  "mms.alltelwireless.com",
+  "txt.att.net",
+  "mms.att.net",
+  "sms.myboostmobile.com",
+  "myboostmobile.com",
+  "txt.att.net",
+  "mms.att.net",
+  "mymetropcs.com",
+  "mymetropcs.com",
+  "msg.fi.google.com",
+  "text.republicwireless.com",
+  "messaging.sprintpcs.com",
+  "pm.sprint.com",
+  "message.ting.com",
+  "tmomail.net",
+  "email.uscc.net",
+  "mms.uscc.net",
+  "vtext.com",
+  "vzwpix.com",
+  "mypixmessages.com",
+  "vmobl.com",
+  "vmpix.com",
+];
 function sleep(milliseconds) {
   const date = Date.now();
   let currentDate = null;
@@ -11,87 +35,79 @@ function sleep(milliseconds) {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
 }
-function usingTheNodemailer(guser, message){
-  console.log("this is the user,", guser)
+function usingTheNodemailer(guser, message) {
+  console.log("this is the user,", guser);
   console.log("this is the message,", message);
   console.log(ignore);
   let theTransporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
+      type: "OAuth2",
       user: guser.email,
       accessToken: guser.access_token,
       refreshToken: guser.refresh_token,
       clientSecret: ignore.web.client_secret,
       clientId: ignore.web.client_id,
     },
-    name: "bob"
+    name: "bob",
   });
 
-  
-  theTransporter.set('oauth2_provision_cb', (user, renew, callback) => {
-    let accessToken = token.access_token;
-    if(!accessToken){
-        return callback(new Error('Unknown user'));
-    }else{
-        return callback(null, accessToken);
+  theTransporter.set("oauth2_provision_cb", (user, renew, callback) => {
+    let accessToken = guser.access_token;
+    if (!accessToken) {
+      return callback(new Error("Unknown user"));
+    } else {
+      return callback(null, accessToken);
     }
-});
-if(message.type === "email"){
-  let destistring = message.destination
+  });
+  if (message.type === "email") {
+    let destistring = message.destination;
     let mailOptions = {
       from: `"phil" ${guser.email}`,
       to: destistring,
       text: message.body,
-   
-  };
-  theTransporter.sendMail(mailOptions, function(error, info){
-    console.log(info)
-    console.log(error)
-    if(error){
-      console.log(error)
-    }else{
-      console.log("email sent: ", info.response)
-      console.log("this is the message", message)
-      sent = true;
-      
-    }
-  })
-  console.log("this is the message", message)
-  
-}else{
-  let sent = false;
-  for(var i = 0; i < providerList.length; i++){
-    if(sent == false){
-      console.log("this is a thing" + message.destination + "@" + providerList[i])
-      let destistring = message.destination + "@" + providerList[i]
-      let mailOptions = {
-        from: `"phil" ${guser.email}`,
-        to: destistring,
-        text: message.body,
-     
     };
-    console.log(mailOptions)
-    theTransporter.sendMail(mailOptions, function(error, info){
-      console.log(info)
-      console.log(error)
-      if(error){
-        console.log(error)
-      }else{
-        console.log("email sent: ", info.response)
-        console.log("this is the message", message)
-        sent = true;
-        
+    theTransporter.sendMail(mailOptions, function(error, info) {
+      console.log(info);
+      console.log(error);
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("email sent: ", info.response);
+        console.log("this is the message", message);
       }
-    })
-    console.log("this is the message", message)
+    });
+    console.log("this is the message", message);
+  } else {
+    let sent = false;
+    for (var i = 0; i < providerList.length; i++) {
+      if (sent == false) {
+        console.log(
+          "this is a thing" + message.destination + "@" + providerList[i]
+        );
+        let destistring = message.destination + "@" + providerList[i];
+        let mailOptions = {
+          from: `"phil" ${guser.email}`,
+          to: destistring,
+          text: message.body,
+        };
+        console.log(mailOptions);
+        theTransporter.sendMail(mailOptions, function(error, info) {
+          console.log(info);
+          console.log(error);
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("email sent: ", info.response);
+            console.log("this is the message", message);
+            sent = true;
+          }
+        });
+        console.log("this is the message", message);
+      }
+      sleep(1000);
     }
-    sleep(1000);
-
-
   }
-}
-
 }
 
 function trySendMessage() {
@@ -101,22 +117,20 @@ function trySendMessage() {
     if (err || !tmessages) {
       console.log(err);
     }
-    
   }).then((messages) => {
-
     for (var i = 0; i < messages.length; i++) {
-      console.log("messages ",messages)
+      console.log("messages ", messages);
 
-      let sendMessage = messages[i]
-      var theUser = ""
-      console.log(Date.parse(messages[i].sendTime) - Date.now())
+      let sendMessage = messages[i];
+      var theUser = "";
+      console.log(Date.parse(messages[i].sendTime) - Date.now());
       console.log(Date.parse(messages[i].sendTime) < Date.now());
-      
+
       if (Date.parse(sendMessage.sendTime) - Date.now() < 0) {
-        GoogleUser.find({id:sendMessage.userId}, (err, users)=>{
-          theUser = users[0]
-        }).then(()=>{
-          usingTheNodemailer(theUser, sendMessage)
+        GoogleUser.find({ id: sendMessage.userId }, (err, users) => {
+          theUser = users[0];
+        }).then(() => {
+          usingTheNodemailer(theUser, sendMessage);
           Message.updateOne(
             { _id: sendMessage._id },
             { $set: { hasdelivered: true } },
@@ -126,12 +140,7 @@ function trySendMessage() {
               console.log(updateOneResponse);
             }
           );
-        })
-        
-        
-          
-        
-        
+        });
       }
     }
   });
